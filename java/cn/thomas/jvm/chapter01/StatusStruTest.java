d98a2676c606f82246b605d3e5291e590b983475
Thomas Ren <isthomasren@gmail.com>
2022-01-16 15:58:43 +0800

    【修改说明】JVM整体架构

---
+++ java/cn/thomas/jvm/chapter01/StatusStruTest.java
@@ -0,0 +1,18 @@
+package cn.thomas.jvm.chapter01;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Program: JVM
+ * @User: Thomas Ren
+ * @Create Time: 2022/1/16 15:32
+ * @Description: 栈的指令集
+ */
+public class StatusStruTest {
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int i = a + b;
+    }
+}
